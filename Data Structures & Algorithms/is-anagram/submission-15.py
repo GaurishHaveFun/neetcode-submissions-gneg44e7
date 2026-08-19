acc49d3c73c8bce1b18c3055cc0be37f2e86835b
GaurishHaveFun <100241276+GaurishHaveFun@users.noreply.github.com>
2026-08-19 16:56:46 -0400

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashmap1 = {}
+        hashmap2 = {}
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            hashmap1[s[i]] = hashmap1.get(s[i], 0) + 1
+            hashmap2[t[i]] = hashmap2.get(t[i], 0) + 1
+        
+        return hashmap1 == hashmap2
